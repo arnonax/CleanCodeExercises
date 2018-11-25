cb2d98479aa712c75f9eda048377bc8cf539f9dc
arnonax <arnonaxelrod@hotmail.com>
2018-11-25 11:16:42 +0200

    Added test WhenUserClicksOnCellHeIsAskedToSelectTowerType

---
+++ TowerDefence/TowerDefence.Tests/GameEngineTests.cs
@@ -0,0 +1,19 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TowerDefence.Tests
+{
+    [TestClass]
+    public class GameEngineTests
+    {
+        [TestMethod]
+        public void WhenUserClicksOnCellHeIsAskedToSelectTowerType()
+        {
+            var mockUI = A.Fake<IGameUI>();
+            var engine = new GameEngine(mockUI);
+
+            engine.UserClickedOnCell(0, 0);
+            A.CallTo(() => mockUI.SelectTowerType(0, 0, engine)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ TowerDefence/TowerDefence.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TowerDefence.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TowerDefence.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e466dd2-d968-42d8-9f52-7f2146eb3c2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
