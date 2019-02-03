324b4db90b61edd181c6c47d1e60e77bf6ea1048
arnonax <arnonaxelrod@hotmail.com>
2019-02-03 18:06:04 +0200

    Adding a unit test project and a first test for Invoice

---
+++ LegacyApplication/LegacyApplication.Tests/InvoiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LegacyApplication.Tests
+{
+    [TestClass]
+    public class InvoiceTests
+    {
+        [TestMethod]
+        public void InitialTotalIs0()
+        {
+            var invoice = new Invoice(A.Fake<IPromotionsCatalog>());
+            var total = invoice.CalculateTotal();
+            Assert.AreEqual(0, total);
+        }
+    }
+}
